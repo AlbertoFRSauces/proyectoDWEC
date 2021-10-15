7d56a08a4863189e4bdf6375cf2f9a8b5f52cbd2
Alberto <alberto.ferram@educa.jcyl.es>
2021-10-15 15:45:52 +0200

    Commit proyectoDWEC.

---
+++ indexProyectoDWEC.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Web Alberto Fernandez Ramirez</title>
+        <meta charset ="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">       
+        <meta name="author" content="Alberto Fernandez Ramirez">
+        <meta name="application-name" content="Proyecto 2021 DAW">
+        <meta name="description" content="Mi pagina web DAW">
+        <meta name="keywords" content="analítica web, seo, web semántica, seo semántico, analytics" >
+        <meta name="robots" content="index, follow">
+        <link href="webroot/css/estilo.css" rel="stylesheet" type="text/css">
+        <link rel="icon" href="../img/home.png" type="image/x-icon">
+    </head>
+    <body>
+        <header>
+            <h1>Desarrollo de Aplicaciones Web</h1>
+        </header>
+        <article class="segundot">
+            <h2>Desarrollo Web Entorno Cliente</h2>
+        </article>
+        <main class="container">
+            
+        </main>
+        <footer class="piepagina">
+            <a href="../index.php"><img src="../webroot/css/img/atras.png" class="imageatras" alt="IconoAtras" /></a>
+            <a href="https://github.com/AlbertoFRSauces/proyectoDAW/tree/master/proyectoDWEC" target="_blank"><img href="https://github.com/AlbertoFRSauces/proyectoDAW/tree/master/proyectoDWEC" target="_blank" src="../webroot/css/img/github.png" class="imagegithub" alt="IconoGitHub" /></a>
+            <p><a>&copy;</a>Alberto Fernández Ramírez 29/09/2021 Todos los derechos reservados.</p>
+            <p>Ultima actualización: 13/10/2021 18:59</p>
+        </footer>
+    </body>
+</html>
